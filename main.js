14420d509207b21bade936565a4952324b464287
bddjr <bddjr08@outlook.com>
2026-08-04 22:15:34 +0800

    2911 bytes

--- main.js
+++ main.js
@@ -5,6 +5,9 @@
 /** @type {"length"} */
 let $length = "length"
 
+/** @type {16} */
+let $16 = 16
+
 let floor = Math.floor
 
 /**
@@ -14,7 +17,7 @@ let floor = Math.floor
  * @param {number} y Second integer
  * @returns {number} Sum
  */
-let safeAdd = (x, y) => 0 | (x >>> 0) + (y >>> 0)
+let safeAdd = (x, y) => 0 | (0 | x) + (0 | y)
 
 /**
  * Bitwise rotate a 32-bit number to the left.
@@ -111,18 +114,18 @@ let binlMD5 = (x, bitLen) => {
     , b = -271733879
     , c = -1732584194
     , d = 271733878
-    , padLenIndex = floor((bitLen + 64) / 512) * 16 + 14
+    , padLenIndex = floor((bitLen + 64) / 512) * $16 + 14
 
   // append padding
   x[floor(bitLen / 32)] |= 0x80 << bitLen % 32;
   x[padLenIndex] = 0 | bitLen;
   x[padLenIndex + 1] = 0 | bitLen / 0x100000000;
 
-  for (; i < x[$length]; i += 16) {
+  for (; i < x[$length]; d = safeAdd(d, oldd)) {
     let [
       x0, x1, x2, x3, x4, x5, x6, x7,
       x8, x9, x10, x11, x12, x13, x14, x15
-    ] = x.slice(i, i + 16)
+    ] = x.slice(i, i += $16)
 
     olda = a
     oldb = b
@@ -165,19 +168,19 @@ let binlMD5 = (x, bitLen) => {
 
     a = md5hh(a, b, c, d, x5, 4, -378558)
     d = md5hh(d, a, b, c, x8, 11, -2022574463)
-    c = md5hh(c, d, a, b, x11, 16, 1839030562)
+    c = md5hh(c, d, a, b, x11, $16, 1839030562)
     b = md5hh(b, c, d, a, x14, 23, -35309556)
     a = md5hh(a, b, c, d, x1, 4, -1530992060)
     d = md5hh(d, a, b, c, x4, 11, 1272893353)
-    c = md5hh(c, d, a, b, x7, 16, -155497632)
+    c = md5hh(c, d, a, b, x7, $16, -155497632)
     b = md5hh(b, c, d, a, x10, 23, -1094730640)
     a = md5hh(a, b, c, d, x13, 4, 681279174)
     d = md5hh(d, a, b, c, x0, 11, -358537222)
-    c = md5hh(c, d, a, b, x3, 16, -722521979)
+    c = md5hh(c, d, a, b, x3, $16, -722521979)
     b = md5hh(b, c, d, a, x6, 23, 76029189)
     a = md5hh(a, b, c, d, x9, 4, -640364487)
     d = md5hh(d, a, b, c, x12, 11, -421815835)
-    c = md5hh(c, d, a, b, x15, 16, 530742520)
+    c = md5hh(c, d, a, b, x15, $16, 530742520)
     b = md5hh(b, c, d, a, x2, 23, -995338651)
 
     a = md5ii(a, b, c, d, x0, 6, -198630844)
@@ -200,7 +203,6 @@ let binlMD5 = (x, bitLen) => {
     a = safeAdd(a, olda)
     b = safeAdd(b, oldb)
     c = safeAdd(c, oldc)
-    d = safeAdd(d, oldd)
   }
   return [a, b, c, d]
 }
@@ -259,18 +261,18 @@ let inputToBytes = (input) => (
 var md5 = (data, key, raw) => {
   var bdata = bytesToBinl(data = inputToBytes(data))
     , bitLen = data[$length] * 8
-    , i = 16
+    , i = $16
     , out = new Uint8Array(i)
 
   if (key != null) {
     // HMAC
     let bkey = bytesToBinl(key = inputToBytes(key))
-      , j = i
-      , opad = Array(j)
+      , i = $16
+      , opad = Array(i)
       , unshift = (/**@type{*}*/ _) => bdata.unshift(...opad)
-    unshift(bkey[$length] > j && (bkey = binlMD5(bkey, key[$length] * 8)))
-    for (; j; opad[j] = bkey[j] ^ 0x5c5c5c5c) {
-      bdata[--j] = bkey[j] ^ 0x36363636
+    unshift(bkey[$length] > $16 && (bkey = binlMD5(bkey, key[$length] * 8)))
+    for (; i; opad[i] = bkey[i] ^ 0x5c5c5c5c) {
+      bdata[--i] = bkey[i] ^ 0x36363636
     }
     unshift(bdata = binlMD5(bdata, 512 + bitLen))
     bitLen = 512 + 128
@@ -285,7 +287,7 @@ var md5 = (data, key, raw) => {
 
   return raw
     ? out
-    : out.reduce((p, v) => p + (v >> 4 && '') + v.toString(16), '')
+    : out.reduce((p, v) => p + (v >> 4 && '') + v.toString($16), '')
 }
 
 export default md5
