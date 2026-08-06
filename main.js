480f98fd942b5789b06d764dfdb8c7bf1bcfb3a2
bddjr <bddjr08@outlook.com>
2026-08-06 11:17:23 +0800

    1196 bytes

--- main.js
+++ main.js
@@ -11,30 +11,30 @@ let $16 = 16
 /** @type {number} */
 let $2_32 = 2 ** 32
 
+let $Array = Array
+
 let $Math = Math
 
 let floor = $Math.floor
 
-/**
- * Add integers, wrapping at 2^32.
- *
- * @param {number} x First integer
- * @param {number} y Second integer
- * @returns {number} Sum
- */
-let safeAdd = (x, y) => 0 | (0 | x) + (0 | y)
+/** @type {((b:number, c:number, d:number) => number)[]} */
+let ff = [
+  (b, c, d) => (b & c) | (~b & d),
+  (b, c, d) => (b & d) | (c & ~d),
+  (b, c, d) => b ^ c ^ d,
+  (b, c, d) => c ^ (b | ~d)
+]
 
-/**
- * Bitwise rotate a 32-bit number to the left.
- *
- * @param {number} num 32-bit number
- * @param {number} cnt Rotation count
- * @returns {number} Rotated number
- */
-let bitRotateLeft = (num, cnt) => (num << cnt) | (num >>> (32 - cnt))
+/** @type {((j: number) => number)[]} */
+let M = [
+  j => j,
+  j => 5 * j + 1 & 15,
+  j => 3 * j + 5 & 15,
+  j => 7 * j & 15
+]
 
 /** @type {number[]} MD5 constants cached in memory */
-let K = Array(64)
+let K = $Array(64)
 
 /**
  * Calculate the MD5 of an array of little-endian words, and a bit length.
@@ -53,164 +53,77 @@ let binlMD5 = (x, bitLen) => {
     , d = 271733878
     , c = ~a
     , b = ~d
+    , j = floor((bitLen + 64) / 512) * $16 + 14
 
   /** @type {number} */
   var k_i
+
+  /** @type {number} */
+  var ff_i
+
+  /** @type {number} */
+  var cmn_t
+
+  /** @type {number[]} */
+  var w = $Array($16)
 
   /**
   * @param {number} q
   * @param {number} a
   * @param {number} b
-  * @param {number} x
   * @param {number} s
-  * @returns {number} Result
+  * @returns {number}
   */
-  var cmn = (q, a, b, x, s) => safeAdd(bitRotateLeft(
-    safeAdd(a, q) + safeAdd(x, K[k_i++] ??= 0 | $2_32 * $Math.abs($Math.sin(k_i))),
-    s
-  ), b)
+  var cmn = (q, a, b, s) => (
+    cmn_t = a + q + w[M[ff_i](j++)] + (K[k_i++] ??= 0 | $2_32 * $Math.abs($Math.sin(k_i))),
+    (cmn_t << s | cmn_t >>> 32 - s) + b
+  )
+
+  /** @type {typeof ff[0]} */
+  var F_f
 
   /**
-   * @param {number} a
-   * @param {number} b
-   * @param {number} c
-   * @param {number} d
-   * @param {number} x
-   * @param {number} s
-   * @returns {number} Result
-   */
-  var ff = (a, b, c, d, x, s) => cmn((b & c) | (~b & d), a, b, x, s)
-
-  /**
-   * @param {number} a
-   * @param {number} b
-   * @param {number} c
-   * @param {number} d
-   * @param {number} x
-   * @param {number} s
-   * @returns {number} Result
-   */
-  var gg = (a, b, c, d, x, s) => cmn((b & d) | (c & ~d), a, b, x, s)
-
-  /**
-   * @param {number} a
-   * @param {number} b
-   * @param {number} c
-   * @param {number} d
-   * @param {number} x
-   * @param {number} s
-   * @returns {number} Result
-   */
-  var hh = (a, b, c, d, x, s) => cmn(b ^ c ^ d, a, b, x, s)
-
-  /**
-   * @param {number} a
-   * @param {number} b
-   * @param {number} c
-   * @param {number} d
-   * @param {number} x
-   * @param {number} s
-   * @returns {number} Result
-   */
-  var ii = (a, b, c, d, x, s) => cmn(c ^ (b | ~d), a, b, x, s)
-
-  var x0, x1, x2, x3, x4, x5, x6, x7,
-    x8, x9, x10, x11, x12, x13, x14, x15
-
-  var X = (/**@type {*}*/ _) => x[i++]
-
-  /**
-   * @param {typeof ff} f
    * @param {number} s0
    * @param {number} s1
    * @param {number} s2
    * @param {number} s3
-   * @param {number} x0
-   * @param {number} x1
-   * @param {number} x2
-   * @param {number} x3
-   * @param {number} x4
-   * @param {number} x5
-   * @param {number} x6
-   * @param {number} x7
-   * @param {number} x8
-   * @param {number} x9
-   * @param {number} x10
-   * @param {number} x11
-   * @param {number} x12
-   * @param {number} x13
-   * @param {number} x14
-   * @param {number} x15
    */
-  var F = (
-    f, s0, s1, s2, s3,
-    x0, x1, x2, x3, x4, x5, x6, x7,
-    x8, x9, x10, x11, x12, x13, x14, x15
-  ) => {
-    /**
-     * @param {number} x0 
-     * @param {number} x1 
-     * @param {number} x2 
-     * @param {number} x3 
-     */
-    var F4 = (x0, x1, x2, x3) => {
-      a = f(a, b, c, d, x0, s0)
-      d = f(d, a, b, c, x1, s1)
-      c = f(c, d, a, b, x2, s2)
-      b = f(b, c, d, a, x3, s3)
+  var F = (s0, s1, s2, s3) => {
+    for (F_f = ff[ff_i], j = 0; j < $16;) {
+      a = cmn(F_f(b, c, d), a, b, s0)
+      d = cmn(F_f(a, b, c), d, a, s1)
+      c = cmn(F_f(d, a, b), c, d, s2)
+      b = cmn(F_f(c, d, a), b, c, s3)
     }
-    F4(x0, x1, x2, x3)
-    F4(x4, x5, x6, x7)
-    F4(x8, x9, x10, x11)
-    F4(x12, x13, x14, x15)
+    ff_i++
   }
-
-  var padLenIndex = floor((bitLen + 64) / 512) * $16 + 14
 
   // append padding
   x[floor(bitLen / 32)] |= 0x80 << bitLen % 32;
-  x[padLenIndex] = 0 | bitLen;
-  x[padLenIndex + 1] = 0 | bitLen / $2_32;
+  x[j] = 0 | bitLen;
+  x[j + 1] = 0 | bitLen / $2_32;
 
-  for (; i < x[$length]; d = safeAdd(d, oldd)) {
+  for (; i < x[$length]; d = 0 | d + oldd) {
     olda = a
     oldb = b
     oldc = c
     oldd = d
 
-    k_i = 0
+    j = k_i = ff_i = 0
 
-    F(ff, 7, 12, 17, 22,
-      x0 = X(), x1 = X(), x2 = X(), x3 = X(),
-      x4 = X(), x5 = X(), x6 = X(), x7 = X(),
-      x8 = X(), x9 = X(), x10 = X(), x11 = X(),
-      x12 = X(), x13 = X(), x14 = X(), x15 = X()
-    )
+    for (; j < $16;) {
+      // x[i++] may be undefined
+      w[j++] = 0 | x[i++]
+    }
 
-    F(gg, 5, 9, 14, 20,
-      x1, x6, x11, x0,
-      x5, x10, x15, x4,
-      x9, x14, x3, x8,
-      x13, x2, x7, x12
-    )
+    F(7, 12, 17, 22)
+    F(5, 9, 14, 20)
+    F(4, 11, $16, 23)
+    F(6, 10, 15, 21)
 
-    F(hh, 4, 11, $16, 23,
-      x5, x8, x11, x14,
-      x1, x4, x7, x10,
-      x13, x0, x3, x6,
-      x9, x12, x15, x2
-    )
-
-    F(ii, 6, 10, 15, 21,
-      x0, x7, x14, x5,
-      x12, x3, x10, x1,
-      x8, x15, x6, x13,
-      x4, x11, x2, x9
-    )
-
-    a = safeAdd(a, olda)
-    b = safeAdd(b, oldb)
-    c = safeAdd(c, oldc)
+    a = 0 | a + olda
+    b = 0 | b + oldb
+    c = 0 | c + oldc
   }
   return [a, b, c, d]
 }
@@ -274,7 +187,7 @@ var md5 = (data, key, raw) => {
   /** @type {*} */
   var bkey
     , pad = (/**@type {number}*/ x) => {
-      bdata.unshift(...Array($16))
+      bdata.unshift(...$Array($16))
       for (; i;) {
         bdata[--i] = x ^ bkey[i]
       }
@@ -302,9 +215,7 @@ var md5 = (data, key, raw) => {
 
   return raw
     ? out
-    : out.toHex
-      ? out.toHex()
-      : out.reduce((p, v) => p + (v >> 4 && '') + v.toString($16), '')
+    : out.toHex?.() ?? out.reduce((p, v) => p + (v >> 4 && '') + v.toString($16), '')
 }
 
 export default md5
