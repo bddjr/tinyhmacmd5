c1af73ccb587a2ae84358476a168d88685f780f3
bddjr <bddjr08@outlook.com>
2026-08-18 08:35:01 +0800

    modified: scripts/_typecheck.ts

--- scripts/_typecheck.ts
+++ scripts/_typecheck.ts
@@ -14,6 +14,11 @@ s = md5('', '', !1)
 s = md5('', '', undefined)
 s = md5('', null, false)
 s = md5('', undefined, false)
+s = md5(Uint8Array.of())
+s = md5(Uint8Array.of(), undefined, false)
+s = md5(Uint8Array.of(), null, false)
+s = md5(Uint8Array.of(), Uint8Array.of(), false)
+s = md5(Uint8ClampedArray.of(), Uint8ClampedArray.of(), false)
 
 let u: Uint8Array<ArrayBuffer>
 
@@ -21,10 +26,20 @@ u = md5('', '', true)
 u = md5('', '', !0)
 u = md5('', null, true)
 u = md5('', undefined, true)
+u = md5(Uint8Array.of(), undefined, true)
+u = md5(Uint8Array.of(), null, true)
+u = md5(Uint8Array.of(), Uint8Array.of(), true)
+u = md5(Uint8ClampedArray.of(), Uint8ClampedArray.of(), true)
 
 let x = md5('', '', Boolean() as boolean)
+
 if (typeof x == 'string') {
     s = x
 } else {
     u = x
 }
+
+x = md5(Uint8Array.of(), undefined, Boolean() as boolean)
+x = md5(Uint8Array.of(), null, Boolean() as boolean)
+x = md5(Uint8Array.of(), Uint8Array.of(), Boolean() as boolean)
+x = md5(Uint8ClampedArray.of(), Uint8ClampedArray.of(), Boolean() as boolean)
